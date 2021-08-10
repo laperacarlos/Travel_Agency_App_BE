6b683526be30da729904e1dafc6b2946cd81e7d1
Karol Gruszczyński <lapera.carlos@gmail.com>
2021-08-10 13:44:25 +0200

    First commit - added files created wtihin new project initialization.

---
+++ src/main/java/com/kodilla/travelagencybe/TravelAgencyBeApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.travelagencybe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelAgencyBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelAgencyBeApplication.class, args);
+    }
+
+}
